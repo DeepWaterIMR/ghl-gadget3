0742cb14f27add24a85f754ee3932bc80300f7bd
MikkoVihtakari <mikko.vihtakari@hi.no>
2023-03-03 08:06:58 +0100

    Widen dl for sexdists

--- 2-3 catch distribution.R
+++ 2-3 catch distribution.R
@@ -168,7 +168,7 @@ if(reload_data) {
         mfdb_interval(
           "len",
           seq(stock_params$minlength, stock_params$maxlength,
-              by = 5*stock_params$dl),
+              by = 2*stock_params$dl),
           open_ended = c("upper","lower")
         ),
       sex = mfdb_group(female = 'F', male = 'M'),
